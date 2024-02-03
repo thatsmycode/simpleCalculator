a5e5ae4eaecfd7caaca1d1ecb0b113f38b0c7b56
Marc.A <marcalberopascual@gmail.com>
2024-02-03 11:28:12 +0100

    data-retrieved-from-form

--- index.php
+++ index.php
@@ -18,6 +18,38 @@
         <input type="text" name="operand3" id="operand3" placeholder="Third input (optional)">
         <button type="submit">=</button>
     </form>
+
+<?php
+
+$userName = "";
+
+$operand_1 = "";
+$operand_2 = "";
+$operand_3 = "";
+$result = "";
+
+$historyArray = [];
+$OperationFound = false;
+$previousUser = "";
+$historyMessage = "This operation has been already done by " . $previousUser;
+
+
+
+
+
+    // Check if the form has been submited and all the required values are present, then proceeds to assign it to the corresponding variables and execute the app logic.
+    if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['userName']) && isset($_GET['operand1']) && isset($_GET['operand2'])) {
+        $userName = $_GET["userName"];
+        $operand_1 = $_GET["operand1"];
+        $operand_2 = $_GET["operand2"];
+
+        if (isset($_GET["operand3"])) {
+            $operand_3 = $_GET["operand3"];
+        }
+    }
+    echo $userName . $operand_1 . $operand_2 . $operand_3;
+?>
+
 </body>
 
 </html>
